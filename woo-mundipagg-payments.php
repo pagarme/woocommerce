9cf8c8c5b897d5c29a0a9c38d53992b7a51b64ac
Michel Lima <michelpl@yahoo.com.br>
2018-03-27 12:31:15 -0300

    :bookmark: Bump version.

--- woo-mundipagg-payments.php
+++ woo-mundipagg-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: WooCommerce MundiPagg Payments
- * Version:     1.0.0
+ * Version:     beta-1.0.10
  * Author:      Mundipagg
  * Author URI:  https://mundipagg.com
  * Text Domain: woo-mundipagg-payments
