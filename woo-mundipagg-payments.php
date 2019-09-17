2057bfa0d162bc05a6ff1129d141e0bf8a14c0d7
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2019-09-17 11:31:51 -0300

    :bookmark: Bump version beta-1.0.21

--- woo-mundipagg-payments.php
+++ woo-mundipagg-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: WooCommerce MundiPagg Payments
- * Version:     beta-1.0.20
+ * Version:     beta-1.0.21
  * Author:      Mundipagg
  * Author URI:  https://mundipagg.com
  * Text Domain: woo-mundipagg-payments
