3c5b2e2e2bcbfb24ef1002ee0d19f392db5be6bb
Wallace Ferreira <wallace.sf87@gmail.com>
2020-07-01 09:52:48 -0300

    🔖 Bump version beta-1.0.23

--- woo-mundipagg-payments.php
+++ woo-mundipagg-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: WooCommerce MundiPagg Payments
- * Version:     beta-1.0.22
+ * Version:     beta-1.0.23
  * Author:      Mundipagg
  * Author URI:  https://mundipagg.com
  * Text Domain: woo-mundipagg-payments
