28b218d85c9e565d51b21c00c4927bcc1ca54118
netorodrigues <jose.oneto@mundipagg.com>
2021-02-05 17:45:36 -0300

    removes last mundipagg words from thank you page

--- templates/thank-you-page.php
+++ templates/thank-you-page.php
@@ -11,14 +11,14 @@ use Woocommerce\Pagarme\View\Checkouts;
 $model = new Order( $order_id );
 $class = 'message';
 
-if ( in_array( $model->mundipagg_status, [ 'failed', 'canceled' ] ) ) {
+if ( in_array( $model->pagarme_status, [ 'failed', 'canceled' ] ) ) {
 	$class = 'error';
 }
 
 ?>
 
 <div class="woocommerce-<?php echo esc_attr( $class ); ?>">
-	<div class="mundipagg-response">
+	<div class="pagarme-response">
 	<?php echo /** phpcs:ignore */ Checkouts::handle_messages( $model ); ?>
 	</div>
 </div>
