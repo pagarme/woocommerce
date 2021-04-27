8ddd4b3a31ede555a734a5d66c46ec39ac3ec796
netorodrigues <jose.oneto@mundipagg.com>
2021-04-27 15:26:18 -0300

    :bug: handle core error response

--- src/Controller/Checkout.php
+++ src/Controller/Checkout.php
@@ -59,6 +59,10 @@ class Checkout
             $fields
         );
 
+        if (!$response) {
+            wp_send_json_error('Failed to create order');
+        }
+
         $order  = new Order($wc_order->get_order_number());
 
         $order->payment_method   = $fields['payment_method'];
