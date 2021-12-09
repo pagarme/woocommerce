f19ddffa0a1f17c4cf2f3657c597037ef445f07a
netorodrigues <jrodrigueson@gmail.com>
2021-12-09 15:42:57 -0300

    🐛 avoids built.js to make requests to tokenize cards

--- assets/javascripts/front/built.js
+++ assets/javascripts/front/built.js
@@ -2874,6 +2874,7 @@ if (window.Sweetalert2) window.sweetAlert = window.swal = window.Sweetalert2;
 	};
 
 	Model.fn.request = function(brand, total, wrapper) {
+        return;
 		var storageName = btoa( brand + total );
 		var storage     = sessionStorage.getItem( storageName );
 		var select 		= wrapper.find( '[data-element=installments]' );
