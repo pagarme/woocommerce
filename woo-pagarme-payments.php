bbe96c6df807bd86856d9a9c5563654e57981a66
M3ndes <thalesmendes.dev@gmail.com>
2022-05-20 12:03:35 -0300

    :bookmark: bump version 2.0.8

--- woo-pagarme-payments.php
+++ woo-pagarme-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Pagar.me module for Woocommerce
- * Version:     2.0.7
+ * Version:     2.0.8
  * Author:      Pagar.me
  * Author URI:  https://pagar.me
  * Text Domain: woo-pagarme-payments
