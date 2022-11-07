ddb25a3c8e474ff0c5679f50e7e9ec286bce890d
mauricio.haygert.ext <mauricio.haygert.ext@infracommerce.com.br>
2022-11-06 22:06:06 -0300

    Menu Pagarme + Paginca de configuracao principal

--- woo-pagarme-payments.php
+++ woo-pagarme-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Pagar.me module for Woocommerce
- * Version:     2.0.14
+ * Version:     3.0.0
  * Author:      Pagar.me
  * Author URI:  https://pagar.me
  * Text Domain: woo-pagarme-payments
