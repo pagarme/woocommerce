fe16a2f33bfbbe5a8670f408602051abf2482297
mauriciohaygert <mauricio.haygert@pagar.me>
2023-01-25 09:51:11 -0300

    Create pagarme_payments.js

--- src/Controller/Gateways/TwoCreditCard.php
+++ src/Controller/Gateways/TwoCreditCard.php
@@ -32,23 +32,15 @@ class TwoCreditCard extends AbstractGateway
     /**
      * @return array
      */
-    public function append_form_fields()
+    public function field_enabled()
     {
         return [
-            'multimethods_2_cards' => $this->field_multimethods_2_cards(),
+            'title'   => __('Enable/Disable', 'woocommerce'),
+            'type' => 'checkbox',
+            'label' => __('Enable multi-means (2 Credit cards)', 'woo-pagarme-payments'),
+            'old_name'    => 'multimethods_2_cards',
+            'default'     => $this->config->getData('multimethods_2_cards') ?? 'no',
         ];
     }
 
-    /**
-     * @return array
-     */
-    public function field_multimethods_2_cards()
-    {
-        return array(
-            'title' => __('Multi-means </br>(2 Credit cards)', 'woo-pagarme-payments'),
-            'type' => 'checkbox',
-            'label' => __('Enable multi-means (2 Credit cards)', 'woo-pagarme-payments'),
-            'default' => 'no',
-        );
-    }
 }
