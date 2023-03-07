2812864ec437be74fc37c19c8c43665a19476cca
Aislan Cedraz <aislan.cedraz@gmail.com>
2023-03-07 14:28:06 -0300

    :bug: WoocommerceCoreSetup wallets

---
+++ assets/javascripts/front/checkout/model/payment/card/wallet.js
@@ -0,0 +1,37 @@
+/* globals wc_pagarme_checkout */
+$ = jQuery;
+const cardSaveTarget = 'select[data-element="choose-credit-card"]';
+const cardFields = [
+    '[data-pagarmecheckout-element="fields-cc-data"]',
+    '[data-element="save-cc-check"]',
+    '[data-element="enable-multicustomers-check"]'
+];
+let pagarmeWallet = {
+    started: false,
+    isStarted: function (){
+        if (!this.started){
+            this.started = true;
+            return false;
+        }
+        return true;
+    },
+    onChangeCard: function (e) {
+        let select  = $( e.currentTarget );
+        let wrapper = select.closest( 'fieldset' );
+        let method  = e.currentTarget.value.trim() ? 'slideUp': 'slideDown';
+        cardFields.forEach( function (field) {
+            wrapper.find(field)[method]();
+        });
+    },
+    addEventListener: function () {
+        $(cardSaveTarget).on('change', function (e) {
+            pagarmeWallet.onChangeCard(e);
+        });
+    },
+    start: function () {
+        if (this.isStarted()) {
+            return;
+        }
+        this.addEventListener();
+    },
+}
