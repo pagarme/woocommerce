d209a9bcc41da4eb350b0c7d200cfe6f2391785c
mauriciohaygert <mauricio.haygert@pagar.me>
2023-03-16 16:11:19 -0300

    validations checkout

--- assets/javascripts/front/checkout/model/multicustomers.js
+++ assets/javascripts/front/checkout/model/multicustomers.js
@@ -14,20 +14,7 @@ jQuery(function ($) {
             if (this.isStarted()) {
                 return;
             }
-            this.addsMask();
             this.addEventListener();
-        },
-        addsMask: function () {
-            // $('.pagarme-card-form-card-number').mask('0000 0000 0000 0000');
-            // $('.pagarme-card-form-card-expiry').mask('00 / 00');
-            // $('.pagarme-card-form-card-cvc').mask('0000');
-            // $('input[name*=\\[cpf\\]]').mask('000.000.000-00');
-            // $('input[name*=\\[zip_code\\]]').mask('00000-000');
-            // $('#billing_cpf').change(function () {
-            //     $('input[name="pagarme[voucher][cards][1][document-holder]"]').empty();
-            //     $('input[name="pagarme[voucher][cards][1][document-holder]"]').val($('#billing_cpf').val()).mask("999.999.999-99").trigger('input');
-            // });
-            // $('input[name="pagarme[voucher][cards][1][document-holder]"]').val($('#billing_cpf').val()).mask("999.999.999-99").trigger('input');
         },
         addEventListener: function () {
             $(multiCustomerTarget).click(function (e) {
@@ -39,7 +26,6 @@ jQuery(function ($) {
                     input.parent().closest('div').find(form)[method]();
                 }
             });
-
         },
     }
 });
