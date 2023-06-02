882e22556a317be1c20c897853ea343b4ececac4
mauriciohaygert <mauricio.haygert@pagar.me>
2023-06-02 17:20:23 -0300

    PAOPN-436 Excesso de chamadas para calcular as parcelas de cartões

--- assets/javascripts/front/checkout/model/payment/card/wallet.js
+++ assets/javascripts/front/checkout/model/payment/card/wallet.js
@@ -8,25 +8,27 @@ const cardFields = [
 ];
 let pagarmeCheckoutWallet = {
     started: false,
-    isStarted: function (){
-        if (!this.started){
+    isStarted: function () {
+        if (!this.started) {
             this.started = true;
             return false;
         }
         return true;
     },
     onChangeCard: function (e) {
-        let select  = $( e.currentTarget );
-        let wrapper = select.closest( 'fieldset' );
-        const method= select.val() ? 'slideUp': 'slideDown';
-        let brand = select.find('option:selected').data('brand');
-        let brandInput = wrapper.find(pagarmeCard.getBrandTarget());
-        brandInput.val(brand);
-        pagarmeCard.updateInstallmentsElement(e);
-        cardFields.forEach( function (field) {
+        let select = $(e.currentTarget);
+        let wrapper = select.closest('fieldset');
+        const method = select.val() ? 'slideUp' : 'slideDown';
+        cardFields.forEach(function (field) {
             wrapper.find(field)[method]();
             wrapper.find(field).find('input').val('');
         });
+        let brand = select.find('option:selected').data('brand');
+        let brandInput = wrapper.find(pagarmeCard.getBrandTarget());
+        brandInput.val(brand);
+        if (select.val()) {
+            pagarmeCard.updateInstallmentsElement(e);
+        }
     },
     addEventListener: function (paymentTarget) {
         paymentTarget = `${paymentTarget} ${cardSaveTarget}`;
