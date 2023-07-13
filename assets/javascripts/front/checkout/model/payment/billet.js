d4712c7e47da48a9ebffe47b8513c2b8780c98b1
RafaMelazzo <rafamelazzo@gmail.com>
2023-07-13 17:58:20 -0300

    Fixing jQuery calls

--- assets/javascripts/front/checkout/model/payment/billet.js
+++ assets/javascripts/front/checkout/model/payment/billet.js
@@ -1,8 +1,6 @@
-$ = jQuery;
-
-$('#pagarme-billet-button').on('click', function(e){
+jQuery('#pagarme-billet-button').on('click', function(e){
     e.preventDefault();
     setTimeout(() => { // Safari is blocking any call to window.open() which is made inside an async call.
-        window.open($(this).attr('data-pagarme-billet-url'), '_blank');
+        window.open(jQuery(this).attr('data-pagarme-billet-url'), '_blank');
     });
 });
