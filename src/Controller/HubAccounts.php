c03fbfc04095a5da3395c624414fcaa4f54a0cf1
Mateus Picoloto <mateus.picoloto@pagar.me>
2024-01-05 13:39:09 -0300

    fix: missing class import

--- src/Controller/HubAccounts.php
+++ src/Controller/HubAccounts.php
@@ -6,11 +6,12 @@ if (!function_exists('add_action')) {
     exit(0);
 }
 
-use Pagarme\Core\Middle\Model\Account;
-use Pagarme\Core\Middle\Model\Account\PaymentMethodSettings;
 use Woocommerce\Pagarme\Helper\Utils;
 use Woocommerce\Pagarme\Model\Config;
+use Pagarme\Core\Middle\Model\Account;
+use Woocommerce\Pagarme\Model\CoreAuth;
 use Woocommerce\Pagarme\Service\AccountService;
+use Pagarme\Core\Middle\Model\Account\PaymentMethodSettings;
 
 class HubAccounts
 {
