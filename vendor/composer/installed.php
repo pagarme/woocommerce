92c76baf6c95594634069ac9be65dee499a6fa98
Mateus Picoloto <38541352+mateus-picoloto@users.noreply.github.com>
2024-03-15 13:56:43 -0300

    chore: change dev to false

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -7,7 +7,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         'apimatic/jsonmapper' => array(
